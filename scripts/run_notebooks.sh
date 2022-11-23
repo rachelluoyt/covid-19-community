7dc32081e4b096f3c146fe0c3b832b4461389c3f
Cloud User <centos@neo4jcovidsandiego-v2.novalocal>
2022-11-23 12:37:11 +0000

    loh

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # This script updates the covid-19-community knowledge graph
-
-cd $COVID19/notebooks/dataprep
  
-LOGDIR="$NEO4J_IMPORT"/logs/`date +%Y-%m-%d`
-mkdir -p "$LOGDIR"
+LOGDIR=/notebooks/dataprep/2022-11-20
 
 # enable conda in bash (see: https://github.com/conda/conda/issues/7980)
 eval "$(conda shell.bash hook)"
 
 # create conda environment
 conda env remove -n covid-19-community &>> $LOGDIR/update.log
-conda env create -f $COVID19/environment.yml &>> $LOGDIR/update.log
+conda env create -f environment.yml &>> $LOGDIR/update.log
 conda activate covid-19-community &>> $LOGDIR/update.log
 
 # run Jupyter Notebooks to download, clean, and standardize data for the knowledge graph
